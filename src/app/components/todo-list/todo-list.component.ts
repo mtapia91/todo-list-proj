3f57b45874a40de123ba4a379acaa76c3a7054cc
mtapia91 <103605583+mtapia91@users.noreply.github.com>
2023-01-06 11:09:51 -0600

    todo list showing

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -32,7 +32,7 @@ export class TodoListComponent implements OnInit{
 
   constructor (private todoService: TodoService) {}
 
-  ngOnInit(): void {
-    this.todoService.todoList.subscribe((TodoItem) => {});
-    }
+  ngOnInit(): void {}
+
+
 }
